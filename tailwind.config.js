e6c68ae86990f2e7e336d06c12594eb166819c29
Sulim63 <Sulim63@example.com>
2024-12-04 21:22:13 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/in/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
